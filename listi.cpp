383b6e7608361d464eb43310d18f4787a60d8b52
MrCiguma <30869062+MrCiguma@users.noreply.github.com>
2017-08-09 19:41:02 +0200

    long long int

--- listi.cpp
+++ listi.cpp
@@ -17,22 +17,18 @@ int main()
 	int l;
 	int i;
 	int j=0;                      //stevilo zasedenih
-	int p = 0;                    //povrsina
-	int option = 0;
-	int saved = 0;
-	int ploscina = 0;
+	long long int option = 0;
+	long long int saved = 0;
+	long long int ploscina = 0;
 	int lokacija;
 	int splitcounter = 0;
-	int counter;
 	int change;
 	int r = 0;                   //stevilo redundant listov
 	static int splits[10000] = { 0 };
-	static int visina[10000] = { 0 };
-	static int sirina[10000] = { 0 };
 	int n;                        //stevilo listov
-	static int listi[2][10000];
-	ifstream ifs("listi07.in");
-	ofstream ofs("listi07.out");
+	static long long int listi[2][10000];
+	ifstream ifs("listi10.in");
+	ofstream ofs("listi10.out");
 	ifs >> n;
 	for (i = 0; i < n; i++){
 		ifs >> listi[0][i] >> listi[1][i];
@@ -65,53 +61,39 @@ int main()
 	do{
 		change = 0;
 		for (j = 0; j <= i-2; j++) {
-			counter = 0;
 			lokacija = 0;
 			saved = listi[0][splits[j]] * listi[1][splits[j+1]-1];
 			l = (splits[j + 1] - splits[j]) - 1;
-			for (k = 0; k < l; k++) {
+			for (k = splits[j]; k < splits[j+1]; k++) {
 				option = listi[0][splits[j]] * (listi[1][k]) + listi[0][k+1] * listi[1][splits[j+1]-1];
 				if (option < saved) {
 					saved = option;
-					lokacija = k + 1 + splits[j];
+					lokacija = k + 1;
 				}
 				else continue;
 			}
-			//ofs << saved << ' ' << lokacija << '\n';
-			//ofs <<'\n' << j << ' ' << k <<' '<< l<<'\n';
-			//for (l = 0; l < n; l++) {
-			//	ofs << splits[l] << ' ';
-			//}
-			//ofs << '\n';
 			if (saved != listi[0][splits[j]] * listi[1][splits[j + 1] - 1]&&saved >0&&lokacija!=0) {
 				splitcounter++;
-				counter++;
 				splits[splitcounter + 1] = lokacija;
-				//ofs << "tu" << ' ' << j << ' ' << k <<' '<<l<<'\n';
 				change = 1;
 			}
 			else continue;
 		}
 		i = splitcounter + 2;
 		sort(splits, splits+i);
-		//ofs << i << '\n';
-
-		//for (l = 0; l < n; l++) {
-		//ofs << splits[l] << ' ';
-		//}
-		//ofs << '\n';
 	} while (change == 1);
 
 	for (j = 0; j <= splitcounter; j++) {
 		ploscina = ploscina + listi[0][splits[j]] * listi[1][splits[j + 1] - 1];
 	}
+	ofs << i << j;
 
 	ofs <<'\n'<< "stevilo kupov je "<<splitcounter+1 <<", skupna ploscina je "<< ' '<<ploscina<<'\n';
 	ofs << "listi po vrsti so:" << '\n';
 	for (i = 0; i < n; i++) {
 		ofs << listi[0][i] << ' '<< listi[1][i] << '\n';
 	}
-	ofs << "spliti so:" << '\n';
+	ofs << "spliti (po katerem listu so kupi loceni) so:" << '\n';
 	for (l = 0; l < splitcounter+2; l++) {
 	ofs << splits[l] << ' ';
 	}
